953195dc19761ae47179a53fda223512dfe44879
Marcos Silva <1609.marcos.silva@gmail.com>
2026-04-25 13:46:18 -0300

    debugging sucess

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "lyra-aurion-system.vercel.app",
+        "https://lyra-aurion-system.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
