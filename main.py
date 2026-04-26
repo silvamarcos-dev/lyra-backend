fdabe83cea2a6107fac77e69bd9b6bb4520bcbfa
Marcos Silva <1609.marcos.silva@gmail.com>
2026-04-25 21:01:01 -0300

    backend running

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173",
+        "http://192.168.0.146:5173",
         "https://lyra-aurion-system.vercel.app",
     ],
     allow_credentials=True,
