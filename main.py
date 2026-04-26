1d874a2c1769b8c0dfad0ca26da00dc1979bb935
Marcos Silva <1609.marcos.silva@gmail.com>
2026-04-26 12:43:53 -0300

    lyra running

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://accommodations-quantities-pets-recommendation.trycloudflare.com",
+        "https://penalty-champagne-lock-arlington.trycloudflare.com",
         "https://lyra-aurion-system.vercel.app",
     ],
     allow_credentials=True,
